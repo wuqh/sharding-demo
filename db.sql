be866ce976ba4a36710f0a8b15bc4021560009e0
rockywoo <wuquanhong@163.com>
2018-01-03 13:42:56 +0800

    建表语句

---
+++ db.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `t_order_0` (
+  `order_id` bigint(64) NOT NULL,
+  `user_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `t_order_1` (
+  `order_id` bigint(64) NOT NULL,
+  `user_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `t_order_2` (
+  `order_id` bigint(64) NOT NULL,
+  `user_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `t_order_3` (
+  `order_id` bigint(64) NOT NULL,
+  `user_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `t_order_4` (
+  `order_id` bigint(64) NOT NULL,
+  `user_id` bigint(64) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
